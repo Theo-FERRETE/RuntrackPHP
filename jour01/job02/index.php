7bf9f8f09f4ab201b18bf9282ee76faf73c1e488
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-13 19:23:56 +0100

    Create index.php

---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+// Affiche "Hello LaPlateforme!" une première fois
+echo "Hello LaPlateforme!<br>";
+
+/*
+Affiche "Hello LaPlateforme!" une deuxième fois
+Utilisation d'un commentaire en bloc pour cette ligne
+*/
+echo "Hello LaPlateforme!";
+?>
