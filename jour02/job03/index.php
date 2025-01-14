2e61533b40af25ad6bed166c06bd49f337a98b05
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-14 14:32:13 +0100

    Update index.php

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -1,14 +1,15 @@
 <?php
 for ($i = 0; $i <= 100; $i++) {
-    if ($i >= 0 && $i <= 20) {
+
+    if ($i == 42) {
+        // Remplacer 42 par "La Plateforme_"
+        echo "La Plateforme_<br>";
+    } elseif ($i >= 0 && $i <= 20) {
         // Afficher en italique
         echo "<i>$i</i><br>";
-    } else if ($i >= 25 && $i <= 50) {
+    } elseif ($i >= 25 && $i <= 50) {
         // Souligner le nombre
         echo "<u>$i</u><br>";
-     echo "La Plateforme_<br>";
-    } else if ($i == 42) {
-        // Remplacer 42 par "La Plateforme_"
     } else {
         // Afficher normalement
         echo "$i<br>";
