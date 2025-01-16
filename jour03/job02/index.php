795c1ff81c7d750ffc42db4cc13393360f66ad47
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-16 10:02:25 +0100

    Update index.php

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -2,8 +2,12 @@
 // Création de la chaîne
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
 
-// Parcours effectué par la chaine
-for ($i = 0; $i < strlen($str); $i += 2) {
-    echo $str[$i];
+// Parcours de la chaine
+$i = 0;
+while (isset($str[$i])) {
+    if ($i % 2 === 0) { 
+        echo $str[$i];
+    }
+    $i++;
 }
 ?>
