2476df72b5a6e879590a01c51562a704fa2cc396
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-16 10:08:38 +0100

    Update Index.php

--- jour03/job03/Index.php
+++ jour03/job03/Index.php
@@ -2,14 +2,19 @@
 // Création de la chaîne
 $str = "I'm sorry Dave I'm afraid I can't do that";
 
-// Définition des voyelles
 $voyelles = "aeiouyAEIOUY";
 
 // Parcours de la chaîne
-for ($i = 0; $i < strlen($str); $i++) {
-    // Vérification si le caractère est une voyelle
-    if (strpos($voyelles, $str[$i]) !== false) {
-        echo $str[$i];
+$i = 0;
+while (isset($str[$i])) { 
+    $j = 0;
+    while (isset($voyelles[$j])) { 
+        if ($str[$i] === $voyelles[$j]) { 
+            echo $str[$i];
+            break; // sortir de la boucle
+        }
+        $j++;
     }
+    $i++;
 }
 ?>
