799404d99dd8a78fc0dcaa843c1cd27a73f19137
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-16 11:13:05 +0100

    Update Index.php

--- jour03/job04/Index.php
+++ jour03/job04/Index.php
@@ -6,11 +6,11 @@ $str = "Dans l'espace, personne ne vous entend crier";
 $compteur = 0;
 
 // Parcours de la chaîne
-for ($i = 0; $i < strlen($str); $i++) {
+while (isset($str[$compteur])) {
     // Incrémentation du compteur
     $compteur++;
 }
 
-// Affichage du résultat
+// résultat
 echo "Le nombre de caractères dans la chaîne est : $compteur";
 ?>
