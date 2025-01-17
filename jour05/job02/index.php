cc7333ad56736230f489a4aecb226ab61e5e22ce
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-01-17 13:55:22 +0100

    Update index.php

--- jour05/job02/index.php
+++ jour05/job02/index.php
@@ -2,9 +2,14 @@
 function bonjour($jour) {
     if ($jour) {
         echo "Bonjour";
+    } else {
+        echo "Bonsoir";
     }
 }
 
-// Appel de la fonction 
-bonjour(true);
+// Appel de la fonction avec true
+bonjour(true);  
+
+// Appel de la fonction avec false
+bonjour(false); 
 ?>
